a23e2011937f8e1f0cc9c0c5df9d79e53eaabccb
Niklas Borglund <niklasborglund@gmail.com>
2021-12-05 19:43:06 +0100

    Update slack event posting

--- start/bugreportersqueezer.js
+++ start/bugreportersqueezer.js
@@ -41,6 +41,7 @@ async function squeezerInit () {
     controller.ready(() =>
     {
         controller.on('bot_message', async(bot, event) => {
+            bot.httpStatus(200)
             return new Promise(async(resolve, reject) => {
                 resolve('resolved!');
                 if (event.attachments !== undefined && event.attachments.length > 0) {
@@ -53,10 +54,67 @@ async function squeezerInit () {
                             fixedTitle = fixedTitle.replace('[MASTER]', '');
                             channel = 'bugreports-master';
                         }
-
+                        
                         await bot.changeContext(event.reference);
-                        var fullSlackMessage = '<' + attachment.title_link + '|' + fixedTitle + '>';
-                        await bot.reply(event, {channel: channel, text: fullSlackMessage, unfurl_links: false});
+
+                        /* TITLE BUILD UP
+                         * 1: report strength emoji
+                         * 2: report strength string
+                         * 3: severity emoji
+                         * 4. severity text
+                         * 5. is sure (bool)
+                         * 6. is cheating (bool)
+                         * 7. title text
+                         */
+                        const splitTitleWords = fixedTitle.split(',');
+                        if(splitTitleWords.length === 7)
+                        {
+                            var reportStrengthEmoji = splitTitleWords[0];
+                            var reportStrengthString = splitTitleWords[1];
+                            var severityEmoji = splitTitleWords[2];
+                            var severityText = splitTitleWords[3];
+                            var isSure = splitTitleWords[4];
+                            var isCheating = splitTitleWords[5];
+                            var titleText = splitTitleWords[6];
+
+                            var strengthString = "Strength " + reportStrengthEmoji + " : " + reportStrengthString;
+                            var severityString = "Severity"
+                            if(isSure === 'not sure')
+                            {
+                                severityString += " (reporter is not sure)";
+                            }
+                            severityString += ": " + severityText + " " + severityEmoji;
+                            
+                            var didCheatString = "";
+                            
+                            if(isCheating === "yes")
+                            {
+                                didCheatString = ". (player has cheats enabled)"
+                            }
+                            
+                            var fullSlackMessage = '<' + attachment.title_link + '|' + titleText + '>';
+                            await bot.reply(event,
+                                {
+                                    channel: channel,
+                                    text: fullSlackMessage,
+                                    unfurl_links: false,
+                                    attachments: [
+                                        {
+                                            text: strengthString + ". " + severityString + didCheatString
+                                        }
+                                    ]
+                                });
+                        }
+                        else
+                        {
+                            var fullSlackMessage = '<' + attachment.title_link + '|' + fixedTitle + '>';
+                            await bot.reply(event,
+                                {
+                                    channel: channel,
+                                    text: fullSlackMessage,
+                                    unfurl_links: false
+                                });
+                        }
                     }
                 }
             });
